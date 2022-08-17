66cb160d8ff8ec6c0e8b8661ef6f00644e6a7a88
caoanhloi <loi26031999@gmail.com>
2022-08-17 16:34:25 +0700

    first commit

---
+++ database/seeders/PostsTableSeerder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeerder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        DB::table('posts')->insert([
+            'name' => 'Admin Admin',
+            'email' => 'admin@argon.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
